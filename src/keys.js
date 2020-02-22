acc48f71e7282ca515734fd5242b2675c61d5b1a
marianozzzz <mariano.zanier@gmail.com>
2020-02-21 21:32:48 -0300

    Initial commit

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports= 
+{
+    mongodb: 
+    {
+        URI: 'mongodb://localhost:27017/users'
+    }
+};
---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/',(req, res, next)=>{ 
+    res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+ });
+router.post('/signup', passport.authenticate('local-signup',{
+    successRedirect:'/profile',
+    failureRedirect: '/signup',
+    passReqToCallBack:true
+}));
+
+router.get('/profile', (req, res, next)=>{
+    res.render('profile');
+});
+
+router.get('/signin', (req, res, next) => {
+    res.render('signin');
+ });
+router.post('/signin', passport.authenticate('local-signin', {
+successRedirect: '/profile',
+failureRedirect: '/signin',
+passReqToCallback: true
+ }));
+
+
+module.exports = router;
